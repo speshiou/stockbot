599a17f4a3f97f2fa7c98a414fa7fcc5cee1397f
pshaung <speshiou@gmail.com>
2020-10-23 15:53:41 +0800

    Remove redundant package

--- stockbot.py
+++ stockbot.py
@@ -3,8 +3,7 @@ import os, sys, datetime
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(ROOT_DIR, "pylib"))
 
-import pandas as pd
-from pandas_datareader import data
+from pandas_datareader import data as pdr
 import yfinance as yf
 
 targets = [ "1101.TW", "1102.TW", "1103.TW", "1104.TW", "1108.TW", "1109.TW" ]
@@ -13,16 +12,15 @@ today = datetime.date.today()
 start = datetime.datetime.now() - datetime.timedelta(days=1095)
 end = datetime.date.today()
 
-pd.core.common.is_list_like = pd.api.types.is_list_like
 yf.pdr_override()
 
-stock = data.get_data_yahoo(targets, start, end)
+data = pdr.get_data_yahoo(targets, start, end)
 buy = []
-for i in stock['Close'].columns:
+for i in data['Close'].columns:
     print(i)
-    deviation_level = stock['Close'][i].mean() - stock['Close'][i].std() * 2
+    deviation_level = data['Close'][i].mean() - data['Close'][i].std() * 2
     print(deviation_level)
-    if deviation_level > stock['Close'][i].iloc[len(stock['Close'][i]) - 1]:
+    if deviation_level > data['Close'][i].iloc[len(data['Close'][i]) - 1]:
         buy.append(i)
 print(buy)
 
